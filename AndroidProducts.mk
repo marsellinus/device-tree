d0d2f5f12d5a3bbdcdeaceb5040b6f57b0961e26
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-09-05 19:57:15 -0700

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/stellar_mojito.mk
+
+COMMON_LUNCH_CHOICES := \
+    stellar_mojito-user \
+    stellar_mojito-userdebug \
+    stellar_mojito-eng

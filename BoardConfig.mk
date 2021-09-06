a3512692e0105b37b84a91dc45319125521a6a57
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-09-06 12:47:03 +0700

    mojito: fix hidl

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,9 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mojito
 TARGET_RECOVERY_DEVICE_MODULES := libinit_mojito
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
